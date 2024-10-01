27ec88905c7c13a15edf867202d253c5e6258b2c
Tarek <119016167+HAMDOUDI-Tarek@users.noreply.github.com>
2024-10-01 16:20:26 +0100

    Graphical enhanced version

--- TIC-TAC-TOE.py
+++ TIC-TAC-TOE.py
@@ -1,134 +1,93 @@
-import time     #the time module is to give the game a realistic aspect, as if the computer is thinking of its next move
+import tkinter as tk
+from tkinter import messagebox
 from random import randrange
 
-def display(board):
-    for i in range(13):
-        if i==0 or i%4==0:
-            print("+","+","+","+",sep=("-------"))
-        elif i==2:
-            print("|",board[0][0],"|",board[0][1],"|",board[0][2],"|",sep=("   "))
-        elif i==6:
-            print("|",board[1][0],"|",board[1][1],"|",board[1][2],"|",sep=("   "))
-        elif i==10:
-            print("|",board[2][0],"|",board[2][1],"|",board[2][2],"|",sep=("   "))
-        else:
-            print("|","|","|","|",sep=("       "))
-    # this function accepts one parameter containing the board's current status
-    # and prints it out to the console.
-    
+# Define the main game class
+class TicTacToe:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tic Tac Toe")
+        
+        # Create the board buttons (3x3 grid)
+        self.buttons = [[None for _ in range(3)] for _ in range(3)]
+        self.board = [[str(3*i+j+1) for j in range(3)] for i in range(3)]
+        self.create_buttons()
 
-def move(board):
-    empty=False
-    move=int(input("enter your move: "))
-    while True:
-        if move not in range(1,10):
-            move=int(input("invalid move! Try again: "))
-        else: break
-    while not empty:
-        if move in range(1,4):
-            i=0
-        elif move in range(4,7):
-            i=1
-        else:
-            i=2
-        if move%3==0:
-            j=2
-        elif move==1 or move==4 or move==7:
-            j=0
-        else:
-            j=1
-        if (i,j) in free_fields(board):
-            board[i][j]="O"
-            empty=True
-        else:
-            move=int(input("field occupied! choose another move: "))
-    time.sleep(2)
-    display(board)
-    # the function accepts the board's current status, asks the user about their move, 
-    # checks the input, and updates the board according to the user's decision.
-    
+    # Create and layout buttons for each cell
+    def create_buttons(self):
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j] = tk.Button(self.root, text=" ", font=('Arial', 24), width=5, height=2,
+                                               command=lambda i=i, j=j: self.user_move(i, j))
+                self.buttons[i][j].grid(row=i, column=j)
+        
+        # The central cell is the first computer move (X)
+        self.board[1][1] = "X"
+        self.animate_move(self.buttons[1][1], "X")
 
+    # Handle user move
+    def user_move(self, i, j):
+        if self.board[i][j] not in ["O", "X"]:
+            self.board[i][j] = "O"
+            self.animate_move(self.buttons[i][j], "O")
+            if self.check_winner("O"):
+                messagebox.showinfo("Tic Tac Toe", "You win!")
+                self.reset_game()
+            elif not self.free_fields():
+                messagebox.showinfo("Tic Tac Toe", "It's a Draw!")
+                self.reset_game()
+            else:
+                self.cpu_move()
 
-def free_fields(board):
-    liste=[]
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] not in ["O","X"]:
-                liste+=(i,j),
-    return liste
-    # this function browses the board and builds a list of all the free cells; 
-    # the list consists of tuples, while each tuple is a pair of row and column numbers.
-    
+    # Handle CPU move
+    def cpu_move(self):
+        empty_cells = self.free_fields()
+        if empty_cells:
+            i, j = empty_cells[randrange(len(empty_cells))]
+            self.board[i][j] = "X"
+            self.animate_move(self.buttons[i][j], "X")
+            if self.check_winner("X"):
+                messagebox.showinfo("Tic Tac Toe", "You lose!")
+                self.reset_game()
+            elif not self.free_fields():
+                messagebox.showinfo("Tic Tac Toe", "It's a Draw!")
+                self.reset_game()
 
-def winner(board, sign):
-    s1=[]
-    s2=[]
-    s3=[]
-    for i in range (1,4):
-        if board[i-1]==sign:
+    # Simple animation to gradually increase text size for a move
+    def animate_move(self, button, sign, size=10):
+        if size <= 24:
+            button.config(text=sign, font=('Arial', size))
+            self.root.after(30, lambda: self.animate_move(button, sign, size + 2))
+        else:
+            button.config(state=tk.DISABLED)
+
+    # Check for winner
+    def check_winner(self, sign):
+        # Check rows, columns, and diagonals
+        for i in range(3):
+            if all(self.board[i][j] == sign for j in range(3)) or \
+               all(self.board[j][i] == sign for j in range(3)):
+                return True
+        if all(self.board[i][i] == sign for i in range(3)) or \
+           all(self.board[i][2-i] == sign for i in range(3)):
             return True
-        s2.append(board[-i][-i])
-        s3.append(board[i-1][3-i])
-    for i in range(3):
-        s1.clear()
-        for j in range(3):
-            s1.append(board[j][i-1])
-        if s1==sign:
-            return True
-    if s2==sign or s3==sign:
-        return True
-    return board
-    # this function analyzes the board's status in order to check if 
-    # the player using 'O's or 'X's has won the game
+        return False
 
+    # Get free fields
+    def free_fields(self):
+        return [(i, j) for i in range(3) for j in range(3) if self.board[i][j] not in ["O", "X"]]
 
-def cpu_move(board):
-    valid=False
-    move=randrange(1,10)
-    print(move)
-    while not valid:
-        if move in range(1,4):
-            i=0
-        elif move in range(4,7):
-            i=1
-        else:
-            i=2
-        if move%3==0:
-            j=2
-        elif move==1 or move==4 or move==7:
-            j=0
-        else:
-            j=1
-        if (i,j) in free_fields(board):
-            board[i][j]="X"
-            valid=True
-        else:
-            print("field occupied! Try another move: ")
-            move=randrange(1,10)
-            print(move)
-    time.sleep(2)
-    display(board)
-    # The function draws the computer's move and updates the board.
-    
+    # Reset the game for a new round
+    def reset_game(self):
+        self.board = [[str(3*i+j+1) for j in range(3)] for i in range(3)]
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j].config(text=" ", state=tk.NORMAL)
+        self.board[1][1] = "X"
+        self.animate_move(self.buttons[1][1], "X")
 
+# Start the Tkinter event loop
 if __name__ == "__main__":
-    board=[[i for i in range(1,4)],[i for i in range(4,7)],[i for i in range (7,10)]]       #initialize the board
-    board[1][1]="X"     #the central cell is the first computer move
-    display(board)      #show the board
-    while True:
-        move(board)
-        sign=["O","O","O"]
-        if winner(board, sign)==True:       #check if the "O"s make a line
-            print("You win!".center(len("you win!")+17))        #output the message in the center
-            break
-        if free_fields(board)==[]:      #check if there are no more empty cells
-            print("Draw!".center(len("draw")+17))
-            break
-        cpu_move(board)
-        sign=["X","X","X"]
-        if winner(board,sign)==True:        #check if the "X"s make a line
-            print("you lose!".center(len("you lose!")+17))
-            break
-        if free_fields(board)==[]:
-            print("Draw!".center(len("draw")+17))
-            break
+    root = tk.Tk()
+    game = TicTacToe(root)
+    root.mainloop()
